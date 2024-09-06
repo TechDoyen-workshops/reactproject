653c67ee176e62eac184453b4ebcd8be16b5847c
unknown <shanuarts1987@gmail.com>
2024-09-06 10:50:34 +0530

    changed bg to class,screen resolution as in tailwind, blackicon and whiteicons to single socialmediaicons

--- src/components/SignIn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SignIn() {
-  return (
-    <div><button className='w-[336px] tab:w-[465.04px] h-[50px] rounded-[2px] bg-[#1F87C9] leading-[20px] text-[15px] font-[700] font-sans hover:bg-[#43b0f3] text-white'>Sign In</button></div>
-  )
-}
-
-export default SignIn
