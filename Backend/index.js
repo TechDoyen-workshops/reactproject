27b6ee9959f818e0c08d0e54dd13019bdbcd54d9
unknown <shanuarts1987@gmail.com>
2024-09-20 17:36:41 +0530

    created a index.js file and new.html file to show the file system modules

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,40 @@
 var http = require('http');
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+
+http.createServer(function (request, response) {
+
+    fs.open('new.html', 'w', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
+    fs.writeFile('new.html', 'Hello content!', function (err) {
+        if (err) throw err;
+        console.log('replaced!');
+    });
+
+    fs.appendFile('new.html', 'Hello wold!', function (err) {
+        if (err) throw err;
+        console.log('upadated!');
+      });
+
+   
+    fs.readFile('new.html', function (err, data) {  
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        return response.end();
+    });
+
+    fs.rename('new.html', 'hello.html', function (err) {
+        if (err) throw err;
+        console.log('filename changed!');
+    });
+
+    // fs.unlink('new.html', function (err) {
+    //     if (err) throw err;
+    //     console.log('File deleted!');
+    //   });
+
+}).listen(8081);
+console.log('Server running at http://127.0.0.1:8081/');
